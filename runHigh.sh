cc87adbe75dca12bb5812097b309d2ea8788164f
Patrick Steiner <Patrick@Steiner-Buchholz.de>
2017-04-16 09:38:01 +0200

    update

---
+++ runHigh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export deviceType="Temperature"
+export deviceID="4711"
+export initialValue="30"
+export count="1"
+export waitTime="1"
+export receiverURL="broker-amq-mqtt-smartgateway.rhel-cdk.10.1.2.2.xip.io"
+export receiverURLPort="30883"
+export brokerUID="admin"
+export brokerPassword="change12_me"
+
+echo "Starting the producer to send messages "
+java -DbrokerUID=$brokerUID -DbrokerPassword=$brokerPassword -DreceiverURL=$receiverURL -DreceiverURLPort=$receiverURLPort -DdeviceType=$deviceType -DdeviceID=$deviceID -DinitialValue=$initialValue -Dcount=$count -DwaitTime=$waitTime -jar Software_Sensor/target/softwareSensor-jar-with-dependencies.jar
